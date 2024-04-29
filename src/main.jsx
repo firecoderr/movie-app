81af6827e293da5f4b3ec4f43dba9874313793e2
firecoderr <akhmedovulugbek296@gmail.com>
2024-04-29 11:14:58 +0500

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,7 @@ import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
-  </React.StrictMode>
 );
